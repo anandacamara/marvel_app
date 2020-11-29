a9d59886ee6583242bfd22bf301688549cd5ed4e
Ananda Câmara <ananda.lr.camara@gmail.com>
2020-11-29 14:23:49 -0300

    Criação da Splash Screen

---
+++ app/src/main/java/com/example/marvelapp/UI/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.marvelapp.UI
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.marvelapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            callActivityLogin()
+            finish()
+        }, 1000)
+    }
+
+    fun callActivityLogin(){
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+}
